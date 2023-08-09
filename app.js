3a3c5fecfa7d706005437c765405cbc45a520557
Maxi Gonzalez <maxiadgonzalez@gmail.com>
2023-08-09 14:41:49 -0300

    config render

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const path = require('path');
 const publicFolderPath = path.resolve(__dirname, './public');
 app.use( express.static(publicFolderPath) );
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen( PORT, () => console.log(`corriendo en el puerto: ${PORT}`))
 
